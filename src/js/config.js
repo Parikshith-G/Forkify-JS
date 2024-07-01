7b2ae2e058c298f88af2777df4e8d8682077a3da
Parikshith G <114581504+Parikshith-G@users.noreply.github.com>
2024-07-01 23:10:10 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RESULTSPERPAGE = 10;
-export const KEY='562e0d28-0525-46bd-981a-0760e025891a'
+export const KEY=
 export const TIMEOUT=2.5
